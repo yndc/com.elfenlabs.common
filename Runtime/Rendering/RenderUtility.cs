00121de8d3f4fdbc765d48d1457d7e4d7baf686c
Yonder <yondercode@gmail.com>
2025-04-09 13:30:18 +0700

    Add RenderUtility

---
+++ Runtime/Rendering/RenderUtility.cs
@@ -0,0 +1,20 @@
+using Unity.Entities;
+using Unity.Rendering;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace Elfenlabs.Rendering
+{
+    public static class RenderUtility
+    {
+        public static BatchMeshID RegisterMesh(World world, UnityEngine.Mesh mesh)
+        {
+            return world.GetExistingSystemManaged<EntitiesGraphicsSystem>().RegisterMesh(mesh);
+        }
+
+        public static BatchMaterialID RegisterMaterial(World world, Material material)
+        {
+            return world.GetExistingSystemManaged<EntitiesGraphicsSystem>().RegisterMaterial(material);
+        }
+    }
+}
